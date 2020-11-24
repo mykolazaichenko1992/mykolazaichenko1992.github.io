07a1e783e0343c5f4f26932731d32b7b5df7e7a6
Mykola Zaichenko <mykolazaichenko1992@gmail.com>
2020-11-24 09:20:09 +0200

    Homework JS2

--- homework20201123/js/main.js
+++ homework20201123/js/main.js
@@ -1,28 +1,15 @@
-let varA = null;
-let varB = null;
-let varC = null;
-let varD = null;
-let varX1 = null;
-let varX2 = null;
-let str = null;
-let tempEL = document.querySelector(".input");
-console.log(tempEL);
-varA = prompt("Введите число А",);
-varB = prompt("Введите число B",);
-varC = prompt("Введите число C",);
-varD = Math.pow(varB, 2) - 4 * varA * varC;
-if(varD > 0){
-  varX1 = (-varB + Math.sqrt(varD)) / 2 * varA;
-  varX2 = (-varB - Math.sqrt(varD)) / 2 * varA;
-  str = "X1 = " + varX1 + "X2 = " + varX2;
-  tempEL.innerHTML = str;
+let str1 = "Первое задание ";
+for (let i = 2; i <= 14; i++){
+  if(i % 2 === 0){ 
+    str1 += i;
+    str1 += " ";
+  }
 }
-else if(varD == 0){
-  varX1 = (-varB) / 2 * varA;
-  str = "X1 = " + varX1;
-  tempEL.innerHTML = str;
+for(i = 33; i <= 45.2; i++){
+  if(i % 2 == 1){
+    str1 += i;
+    str1 += " ";
+  }
 }
-else{
-  str = "У уравнения нету кореней";
-  tempEL.innerHTML = str;
-}
+let zadanie1 = document.querySelector(".main");
+zadanie1.innerHTML = str1;
