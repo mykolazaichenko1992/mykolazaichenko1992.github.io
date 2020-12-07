a9285c20d577813ba312bfa07cbdebf79b7268b3
Mykola Zaichenko <mykolazaichenko1992@gmail.com>
2020-12-07 18:46:31 +0200

    homework

--- homework20201121/js/main.js
+++ homework20201121/js/main.js
@@ -5,24 +5,23 @@ let varD = null;
 let varX1 = null;
 let varX2 = null;
 let str = null;
-let tempEL = document.querySelector(".input");
-console.log(tempEL);
-varA = prompt("Введите число А",);
-varB = prompt("Введите число B",);
-varC = prompt("Введите число C",);
+let tempEL = document.querySelector(`.input`);
+varA = 3;
+varB = 20;
+varC = 30;
 varD = Math.pow(varB, 2) - 4 * varA * varC;
 if(varD > 0){
   varX1 = (-varB + Math.sqrt(varD)) / 2 * varA;
   varX2 = (-varB - Math.sqrt(varD)) / 2 * varA;
-  str = "X1 = " + varX1 + "X2 = " + varX2;
+  str = `X1 = ${varX1}<br> X2 = ${varX2}`;
   tempEL.innerHTML = str;
 }
 else if(varD == 0){
   varX1 = (-varB) / 2 * varA;
-  str = "X1 = " + varX1;
+  str = `X1 = ${varX1}`
   tempEL.innerHTML = str;
 }
 else{
-  str = "У уравнения нету кореней";
+  str = `У уравнения нету кореней`;
   tempEL.innerHTML = str;
 }
