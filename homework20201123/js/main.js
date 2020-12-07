c8e21805a8345389b1514b5107252aa2e2d5b261
Mykola Zaichenko <mykolazaichenko1992@gmail.com>
2020-12-07 18:38:07 +0200

    homework

--- homework20201123/js/main.js
+++ homework20201123/js/main.js
@@ -1,10 +1,10 @@
 let str1 = "Первое задание ";
 
 for(let i = 2; i <= 14; i += 2){
-  str1 += i + " ";
+  str1 = `${str1} ${i}`;
 }
 for(let i = 33; i <= 45.2; i += 2){
-  str1 += i + " ";
+  str1 = `${str1} ${i}`;
 }
 
 let zadanie1 = document.querySelector(".main");
