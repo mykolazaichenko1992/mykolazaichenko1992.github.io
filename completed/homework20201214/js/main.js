307ab2ef614a5075a1efe437994b1348ef38c6dd
Mykola Zaichenko <mykolazaichenko1992@gmail.com>
2021-01-11 11:00:10 +0200

    Homework

--- completed/homework20201214/js/main.js
+++ completed/homework20201214/js/main.js
@@ -1,18 +1,24 @@
 const findTextArea = document.querySelector(`.textarea`);
 const findButton = document.querySelector(`.button`);
 const findOutPut = document.querySelector(`.output`);
+
 let str = new String();
 let strArray = new String();
+let longestWord = new String();
 
 const createTextAreas = () => {
   strArray = strArray.split(` `);
+  strArray = strArray.filter((element) => {
+    return element != ``;
+  })
   console.log(strArray);
   strArray.forEach(element => {
     str += `<input value="${element}"</input>`;
   });
   findOutPut.innerHTML = str;
 }
-findButton.addEventListener(`click`, () =>{
+
+findButton.addEventListener(`click`, () => {
   strArray = findTextArea.value;
   createTextAreas();
 })
---
+++ homework20201221/js/main.js
@@ -0,0 +1,6 @@
+let findButtonLamp = document.querySelector(`.buttonLamp`);
+let findLamp = document.querySelector(`.lamp-off`)
+findButtonLamp.addEventListener(`click,` = () => {
+  findLamp.remove(`.lamp-on`);
+  findLamp.addEventListener()
+});
